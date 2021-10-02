cc660808b2bbb699be0b537b08310c67ca7612fa
Ilya Chvanov <mercy1ess@list.ru>
2021-10-02 12:52:41 +0300

    Fix randomArray.js

--- arrNoRepeat.js
+++ arrNoRepeat.js
@@ -4,19 +4,22 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function isArray() {
+function isRandomArray() {
     var arr = [];
     var arrCount = 6;
-    var min = 1, max = 9, n;
+    var min = 1, max = 9;
     for (var i = 0; i < arrCount; i++) {
-        n = getRandomInt(min, max);
-        if (!arr.includes(n)) {
-            arr.push(n);
-        } else {
-            i--;
+        var n = getRandomInt(min, max);
+        while (true) {
+            if(!arr.includes(n)) {
+                arr.push(n);
+                break;
+            } else {
+                n = getRandomInt(min, max);
+            }
         }
     }
     return arr;
 }
 
-console.log(isArray());
+console.log(isRandomArray());
