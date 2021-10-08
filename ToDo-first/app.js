c71eafe3b4f9d644c33bf00dfeca0bb0f6ef9a6e
Ilya Chvanov <mercy1ess@list.ru>
2021-10-08 12:32:17 +0300

    Completed todo

--- ToDo-first/app.js
+++ ToDo-first/app.js
@@ -1,37 +1,56 @@
 var btn = document.querySelector('.todo__btn'),
-    svg = document.getElementsByTagName('svg')[0],
+    svg = document.getElementsByTagName('svg'),
     menu = document.querySelector('#todo__menu'),
     clear = document.querySelector('.clear__btn'),
     countP = document.querySelector('.todo__counter'),
     spanCheck = document.querySelector('.todo__check-clear'),
     counterCheckColor = document.querySelector('.todo__counter-color-check'),
-    count = 0, degCount = 0;
+    input = document.querySelector('.todo__input'),
+    count = degCount = 0;
 
 function addTask() {
-    var input = document.querySelector('.todo__input'),
-        div = document.createElement('div'),
-        p = document.createElement('p');
+    var div = document.createElement('div'),
+        p = document.createElement('p'),
+        spanDel = document.createElement('span');
     
     if (input.value != '') {
         div.className = 'menu__task';
         p.className = 'task';
+        spanDel.className = 'task-del';
         
         menu.appendChild(div);
         div.appendChild(p);
+        div.appendChild(spanDel);
         p.innerHTML = input.value;
 
         input.value = '';
+
+        spanDel.innerHTML = '<img src="task-del.png" alt="del">';
 
         input.classList.remove('todo__input-place__color');
         input.placeholder = 'Write your task';
 
         degCount += 360;
-        svg.style.transform = 'rotate(' + degCount + 'deg)';
+        svg[0].style.transform = 'rotate(' + degCount + 'deg)';
         countP.innerHTML = 'Counter tasks: ' + ++count;
 
-        counterCheckColor.style.color = '#260026';
+        countP.style.color = '#260026';
         clear.style.background = '#260026';
         spanCheck.style.display = 'none';
+
+        div.onclick = function () {
+            if (div.className != 'menu__task checked') {
+                div.classList.add('checked');
+                countP.innerHTML = 'Counter tasks: ' + count;
+            } else {
+                div.classList.remove('checked');
+            }
+        };
+
+        spanDel.onclick = function() {
+            div.remove();
+            countP.innerHTML = 'Counter tasks: ' + --count;
+        };
     } else {
         input.classList.add('todo__input-place__color');
         input.placeholder = 'ERROR! Write task!';
@@ -47,7 +66,7 @@ clear.addEventListener('click', function() {
         count = 0;
         countP.innerHTML = 'Counter tasks: ' + count;
     } else {
-        counterCheckColor.style.color = '#f00';
+        countP.style.color = '#f00';
         clear.style.background = '#f00';
         spanCheck.style.display = 'block';
     }
