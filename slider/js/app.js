0b5d51630900b91e5d874c4732aef78b0cd374e0
Ilya Chvanov <mercy1ess@list.ru>
2021-10-12 13:25:34 +0300

    Changed slider

--- slider/js/app.js
+++ slider/js/app.js
@@ -1,15 +1,16 @@
-var sliderLine = document.querySelector('.slider__box-line'),
+var sliderLine = document.querySelector('.slider__line'),
     items = document.querySelectorAll('.item'),
-    itemSize = 600, count = a = 0;
+    sliderContentSize = document.querySelector('.slider__content').offsetWidth - 4,
+    count = a = 0;
 
 document.getElementById('prev').addEventListener('click', function() {
-    count = count + itemSize;
+    count = count + sliderContentSize;
     if (count > 0) {
-        count = (items.length - 1) * -itemSize;
+        count = (items.length - 1) * -sliderContentSize;
     }
 
     for (var item of items) {
-        if (item.classList == 'item item-switch') {
+        if (item.classList.contains('item-switch')) {
             item.classList.remove('item-switch');
             a--;
             if (a < 0) {
@@ -26,13 +27,13 @@ document.getElementById('prev').addEventListener('click', function() {
 });
 
 document.getElementById('next').addEventListener('click', function() {
-    count = count - itemSize;
-    if (count < (items.length - 1) * -itemSize) {
+    count = count - sliderContentSize;
+    if (count < (items.length - 1) * -sliderContentSize) {
         count = 0;
     }
 
     for (var item of items) {
-        if (item.classList == 'item item-switch') {
+        if (item.classList.contains('item-switch')) {
             item.classList.remove('item-switch');
             a++;
             if (a < items.length) {
@@ -52,14 +53,14 @@ for (var i = 0; i < items.length; i++) { //e.targer = this
     items[i].setAttribute('id', i + 1);
     items[i].onclick = function(e) {
         for (var item of items) {
-            if (item.classList == 'item item-switch') {
+            if (item.classList.contains('item-switch')) {
                 item.classList.remove('item-switch');
                 break;
             }
         }
         a = this.id - 1;
         items[a].classList.add('item-switch');
-        count = (this.id - 1) * -itemSize;
+        count = (this.id - 1) * -sliderContentSize;
         sliderLine.style.transform = 'translateX(' + count + 'px)';
     }
 }
