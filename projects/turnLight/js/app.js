3c5a9c9ef6431536c56ff79dfa548e0573ebca11
Ilya Chvanov <mercy1ess@list.ru>
2021-10-13 01:01:07 +0300

    fix random bg color file (turn on/off)

--- projects/turnLight/js/app.js
+++ projects/turnLight/js/app.js
@@ -1,25 +1,29 @@
 var body = document.getElementsByTagName('body')[0],
     hex = document.querySelector('#hex-code'),
-    fullCode = '', num;
+    fullCode = '', count = 0, num;
 
 var arrayCode = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                  'A', 'B', 'C', 'D', 'E', 'F'];
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max); // 0 - 16(не включительно)
 }
 
 document.querySelector('#btn-on').addEventListener('click', function() {
     for (var i = 0; i < 6; i++) {
-        num = getRandomInt(arrayCode.length);
+        num = getRandomInt(arrayCode.length); // 16
         fullCode += arrayCode[num];
     }
     document.getElementById('hex-code').innerText = '#' + fullCode;
     body.style.background = '#' + fullCode;
     fullCode = '';
+
+    console.log(++count);
 });
 
 document.querySelector('#btn-off').addEventListener('click', function() {
     document.getElementById('hex-code').innerText = '#050801';
     body.style.background = '#050801';
+
+    count = 0;
 });
